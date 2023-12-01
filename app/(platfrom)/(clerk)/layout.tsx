18d3471130ccea0c6b2eb1e578b0169fb95f3a17
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2023-12-01 16:05:06 +0530

    organization implemented (#3)

--- app/(platfrom)/(clerk)/layout.tsx
+++ app/(platfrom)/(clerk)/layout.tsx
@@ -1,9 +1,7 @@
-const ClerkLayout = ({ children }:{children:React.ReactNode}) => {
-    return (
-        <div className="h-full justify-center flex items-center">
-            {children}
-        </div>
-    );
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full justify-center flex items-center">{children}</div>
+  );
 };
 
 export default ClerkLayout;
