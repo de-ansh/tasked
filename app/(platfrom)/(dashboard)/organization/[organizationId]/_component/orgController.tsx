eeb27e8b8b840cee825cd7e95e122461e7c9c4c4
Ashish Mishra <anshm9448@gmail.com>
2023-12-03 16:14:52 +0530

    sideBar

--- app/(platfrom)/(dashboard)/organization/[organizationId]/_component/orgController.tsx
+++ app/(platfrom)/(dashboard)/organization/[organizationId]/_component/orgController.tsx
@@ -3,12 +3,11 @@ import { useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useOrganizationList } from "@clerk/nextjs";
 export const OrgControl = () => {
-    const params = useParams();
-    const {setActive}= useOrganizationList();
-    useEffect(() => {
-        if(!setActive) return;
-        setActive({ organization: params.organizationId as string});
-    }, [setActive,params.organizationId]);
-    return null;
-}
- 
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+  useEffect(() => {
+    if (!setActive) return;
+    setActive({ organization: params.organizationId as string });
+  }, [setActive, params.organizationId]);
+  return null;
+};
